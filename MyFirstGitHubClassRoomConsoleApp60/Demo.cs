fac928037340559167674202b51d040c8e3f4c26
SHUCAYB-AKA-SHUSHU <93126733+SHUCAYB-AKA-SHUSHU@users.noreply.github.com>
2022-09-05 12:41:03 +0200

    propperties

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		private string Name;
+
+	public string Name
+	{
+		get { return Name; }
+		set { Name = value; }
+	}
+
+	private int age;
+
+	public int Age
+	{
+		get { return age; }
+		set { age = value; }
+	}
+
+	
+
+}
+}
